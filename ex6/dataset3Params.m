16f4a094040f0142a733a902bc6e75602143b539
Kurt Kretzer <kurt.kretzer@gmail.com>
2012-06-08 07:33:17 -0500

    first half of ex6 done

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -24,10 +24,23 @@ sigma = 0.3;
 %
 
 
+range = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30](:);
+range_size = size(range, 1);
+prediction_error = zeros(range_size, range_size);
 
+for try_C = 1:range_size
+    for try_sigma = 1:range_size
+        model = svmTrain(X, y, range(try_C, 1), @(x1, x2) gaussianKernel(x1, x2, range(try_sigma, 1)));
+        predictions = svmPredict(model, Xval);
+        prediction_error(try_C, try_sigma) = mean(double(predictions ~= yval));
+    endfor
+endfor
 
-
-
+[col_mins, col_mins_idx] = min(prediction_error);
+[min_min, sigma_min_idx] = min(col_mins);
+C_min_idx = col_mins_idx(sigma_min_idx);
+C = range(C_min_idx);
+sigma = range(sigma_min_idx);
 
 % =========================================================================
 
